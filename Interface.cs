827cbb1f1aee44a62c0a9f4dcdb7cd622fd69b0d
Mahesh <Maheshk2104@gmail.com>
2023-06-19 10:43:14 +0530

    [Mahesh]Add: Interface Examples

---
+++ Interface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept 
+{
+    interface Interface_One
+    {
+        // Interface method does not have any body
+        // We can create multiple types of bodies
+        // •Interface is a complete abstract class because all the methods which are declared inside the interface are abstract methods.
+        // •It cannot have method body.
+        // •Interface methods are public or abstract by default.
+        // •It's implementation must done by class or strut.
+        // •It is used to achieve multiple inheritance which can't be achieved by class
+        void one();
+    }
+    interface Interface_Two
+    {
+        void two();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OopsConcept
+{
+    internal class Program : Interface_One, Interface_Two
+    {
+        static void Main(string[] args)
+        {
+            Interface_One interface_One = new Program();
+            interface_One.one();
+            Interface_Two interface_Two = new Program();
+            interface_Two.two();
+            Console.ReadLine();
+            InterfaceClassTwo two = new InterfaceClassTwo();
+            two.one();
+        }
+
+        public void one()
+        {
+            Console.WriteLine("Mahesh");
+        }
+
+        public void two()
+        {
+            Console.WriteLine("Kumar");
+        }
+    }
+}
